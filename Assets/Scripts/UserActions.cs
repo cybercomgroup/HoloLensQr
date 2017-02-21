863d51842c1356233e48b9de1a9465715c0fb1d5
Gustav Svensson <gustav.svensson@cybercom.com>
2017-02-21 15:32:23 +0100

    Speach commands and UI cleanup

---
+++ Assets/Scripts/UserActions.cs
@@ -0,0 +1,59 @@
+﻿using HoloToolkit.Unity;
+using HoloToolkit.Unity.InputModule;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserActions : Singleton<UserActions> {
+
+    public MenuScript movingMenu = null;
+    
+    //Starts scanning 
+    public void ScanQR()
+    {
+        Backend.Instance.GetComponent<QrScan>().OnScan();
+    }
+
+    //Closes the (if any) gazed at menu
+    public void CloseMenu()
+    {
+        MenuScript theMenu = GazeMenu();
+        if (theMenu != null)
+        {
+            theMenu.close();
+        }
+    }
+
+    //Starts movig the (if any) gazed at menu
+    public void MoveMenu()
+    {
+        MenuScript theMenu = GazeMenu();
+        if (theMenu != null)
+        {
+            theMenu.startMove();
+        }
+    }
+
+    private MenuScript GazeMenu()
+    {
+        if (GazeManager.Instance.IsGazingAtObject)
+        {
+            GameObject g = GazeManager.Instance.HitObject;
+            if (g != null)
+            {
+                return g.GetComponentInParent<MenuScript>();
+            }
+        }
+        return null;
+    }
+
+    //Places the currently (if any) moving menu
+    public void PlaceMenu()
+    {
+        if (movingMenu != null)
+        {
+            movingMenu.stopMove();
+        }
+        //movingMenu = null;
+    }
+}
